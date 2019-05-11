e0a2891f8187ceaa1c2ba98d70bfcb7d1901b37b
Maciej Wasik <maciej.wasik1@gmail.com>
2019-05-11 13:08:28 +0200

    Updates

---
+++ precache-manifest.6e6995fa3fc609224fe78fd7179a9823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681c05fb6fa34c8074a3674c98f56bc8",
+    "url": "/svg-animation-editor/index.html"
+  },
+  {
+    "revision": "81e0a47350b717b87d77",
+    "url": "/svg-animation-editor/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/svg-animation-editor/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "81e0a47350b717b87d77",
+    "url": "/svg-animation-editor/static/js/main.a66cfaf6.chunk.js"
+  },
+  {
+    "revision": "baaee94267b0620ba2bf",
+    "url": "/svg-animation-editor/static/js/runtime~main.a709a186.js"
+  }
+]);
