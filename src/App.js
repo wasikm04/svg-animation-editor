d86d972bbfce6a15198640f6a02b5e5c2d639653
Maciej Wasik <maciej.wasik1@gmail.com>
2019-05-11 13:36:11 +0200

    Editing GitHub Pages configuration

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         SVG Animation Editor
       </header>
+      <footer>
+      <p>Authors: Maciej Wasik, Jakub Rejent</p>
+      </footer>
+      
     </div>
   );
 }
