c3b798d32e6b862647a2eef919b2f623a7f2145a
Jakub Rejent <kubarej1@gmail.com>
2019-05-12 15:34:47 +0200

    Add function showing editing element name

--- src/EditionPanel.js
+++ src/EditionPanel.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+function EditingTitle(props) {
+  return (
+    <p>Edytujesz element: <strong>{props.elemName}</strong></p>
+  );
+}
+
+function EditingTitle(props) {
+  return (
+    <p>Edytujesz element: <strong>{props.elemName}</strong></p>
+  );
+}
+
+
 class EditionPanel extends React.Component {
 
     render() {
       return (      
+        <div className="container">
         <div className="row">
-            panel edycji
+                <EditingTitle
+        elemName="Element 1" />
+        </div>
+        <div className="row">test</div>
         </div>
       );
     }
