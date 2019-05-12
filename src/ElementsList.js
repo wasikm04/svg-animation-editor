a6d057ebdbe1544b56437ae796b86d7aaac7dc76
Maciej Wasik <maciej.wasik1@gmail.com>
2019-05-12 13:15:04 +0200

    Script files included

--- src/ElementsList.js
+++ src/ElementsList.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
 class ElementsList extends React.Component {
-
     render() {
       return (       
-        <div className="row">
-Lista z importami
+        <div classNameName="container">
+          <h2>SVG Editor</h2>
+          <br></br>
+          <ul className="nav nav-tabs" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link active" data-toggle="tab" href="#list">Lista Elementów</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="tab" href="#import">Import</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="tab" href="#export">Export</a>
+              </li>
+          </ul>
+          <div className="tab-content">
+            <div id="list" className="container tab-pane active">          
+              <h3>Elementy</h3>
+              <p>Lista mapowana z tablicy z propsów</p>
+            </div>
+            <div id="import" className="container tab-pane fade">    
+            <h3>Importuj plik SVG</h3>
+              <p>import z onChange podpiętym do funkcji przekazanej z App.js</p>     
+            </div>
+            <div id="export" className="container tab-pane fade">  
+            <h3>Eksportuj plik SVG</h3>
+              <p>Zrzut całego state do pliku</p>  
+            </div>
+          </div>
         </div>
       );
     }
