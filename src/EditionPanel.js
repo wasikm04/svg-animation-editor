e26322c8c6c0b4c6d6628765da2565c730e2ab40
Jakub Rejent <kubarej1@gmail.com>
2019-05-12 16:06:40 +0200

    Add FieldInput control to change parameters with state

--- src/EditionPanel.js
+++ src/EditionPanel.js
@@ -6,14 +6,44 @@ function EditingTitle(props) {
   );
 }
 
-function EditingTitle(props) {
-  return (
-    <p>Edytujesz element: <strong>{props.elemName}</strong></p>
-  );
+class FiledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onValueChange(e.target.value);
+  }
+
+  render() {
+    const valueChange = this.props.valueChange;
+    const title = this.props.fieldName;
+    return (
+      <div className="row">
+      <fieldset>
+        <legend>{title}</legend>
+        <input value={valueChange}
+               onChange={this.handleChange} />
+              
+      </fieldset>
+      </div>
+    );
+  }
+
 }
 
-
 class EditionPanel extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleXPositionChange = this.handleXPositionChange.bind(this);
+    this.state = {xposition: 50};
+  }
+
+  handleXPositionChange(valueChange) {
+    this.setState({xposition: valueChange});
+  }
 
     render() {
       return (      
@@ -22,8 +52,12 @@ class EditionPanel extends React.Component {
                 <EditingTitle
         elemName="Element 1" />
         </div>
-        <div className="row">test</div>
-        </div>
+        <FiledInput
+          valueChange={this.state.xposition}
+          fieldName= 'Change X Postion: '
+          onValueChange={this.handleXPositionChange} />
+          <p>posX: {this.state.xposition}</p>
+          </div>
       );
     }
   }
