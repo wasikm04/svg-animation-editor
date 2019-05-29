a706a9212eb4ce4132b77630d1f97fc4554626a2
Maciej Wasik <maciej.wasik1@gmail.com>
2019-05-29 20:00:45 +0200

    New animation included in list

--- src/helpers/AnimationsList.js
+++ src/helpers/AnimationsList.js
@@ -9,67 +9,39 @@ class AnimationsList extends React.Component {
 
   createAnimationsList(svg) {
     var resultArr = [];
-    if (svg.animate && Array.isArray(svg.animate)) {
-      for (var elem in svg.animate) {
-        if (svg.animate[elem]._attributes && svg.animate[elem]._attributes.id) {
-          resultArr.push(
-            <option
-              key={elem}
-              value={svg.animate[elem]._attributes.id}
-              className="col-md"
-            >
-              {svg.animate[elem]._attributes.id}
-            </option>
-          );
-        }
-      }
-    } else if (
-      svg.animate &&
-      svg.animate._attributes &&
-      svg.animate._attributes.id
-    ) {
-      resultArr.push(
-        <option
-          key={svg.animate._attributes.id}
-          value={svg.animate._attributes.id}
-          className="col-md"
-        >
-          {svg.animate._attributes.id}
-        </option>
-      );
-    }
-    if (svg.animateTransform && Array.isArray(svg.animateTransform)) {
-      for (elem in svg.animateTransform) {
-        if (
-          svg.animateTransform[elem]._attributes &&
-          svg.animateTransform[elem]._attributes.id
+    for (var anim in svg) {
+      if(["animate","animateTransform","animateMotion"].includes(anim)){
+        if (svg[anim] && Array.isArray(svg[anim])) {
+          for (var elem in svg[anim]) {
+            if (svg[anim][elem]._attributes && svg[anim][elem]._attributes.id) {
+              resultArr.push(
+                <option
+                  key={elem}
+                  value={svg[anim][elem]._attributes.id}
+                  className="col-md"
+                >
+                  {svg[anim][elem]._attributes.id}
+                </option>
+              );
+            }
+          }
+        } else if (
+          svg[anim] &&
+          svg[anim]._attributes &&
+          svg[anim]._attributes.id
         ) {
           resultArr.push(
             <option
-              key={elem}
-              value={svg.animateTransform[elem]._attributes.id}
+              key={svg[anim]._attributes.id}
+              value={svg[anim]._attributes.id}
               className="col-md"
             >
-              {svg.animateTransform[elem]._attributes.id}
+              {svg[anim]._attributes.id}
             </option>
           );
         }
-      }
-    } else if (
-      svg.animateTransform &&
-      svg.animateTransform._attributes &&
-      svg.animateTransform._attributes.id
-    ) {
-      resultArr.push(
-        <option
-          key={svg.animateTransform._attributes.id}
-          value={svg.animateTransform._attributes.id}
-          className="col-md"
-        >
-          {svg.animateTransform._attributes.id}
-        </option>
-      );
     }
+  }
     return resultArr;
   }
 
